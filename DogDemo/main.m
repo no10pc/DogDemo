35f496ab045342fe35dd44a024c398ab2135af1e
no10pc <no10pc@gmail.com>
2013-05-09 00:40:58 +0800

    Initial Commit

---
+++ DogDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DogDemo
+//
+//  Created by 付 翾宇 on 13-5-9.
+//  Copyright (c) 2013年 no10pc@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
